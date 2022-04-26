259fbbda6afc5f67c9dbdca30301858310ecbc00
Semiatom <103575032+Semiatom@users.noreply.github.com>
2022-04-26 20:32:12 +0300

    rev.1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,23 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld beanHello1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(beanHello1.getMessage());
+
+        HelloWorld beanHello2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanHello2.getMessage());
+
+        Cat beanCat1 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat1.getMessage());
+
+        Cat beanCat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat2.getMessage());
+
+        System.out.println(beanHello1 == beanHello2);
+        System.out.println((beanCat1 == beanCat2));
     }
 }
